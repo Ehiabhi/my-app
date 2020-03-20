be78533e3992136ab1ccba37ea02f722db12abfb
Ehiabhi <enimakpokpo@yahoo.com>
2020-03-20 13:30:26 +0100

    Updates

---
+++ precache-manifest.93517bb861470e11c60dc83e069a431d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72c152b9d93494bdfe461e3c562bda5e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "681740fc0ab32a13f6ce",
+    "url": "/my-app/static/js/2.e115a758.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-app/static/js/2.e115a758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6c39acf72072aa418f2",
+    "url": "/my-app/static/js/main.28f0fda6.chunk.js"
+  },
+  {
+    "revision": "1b58fcb5ce8d26145ee7",
+    "url": "/my-app/static/js/runtime-main.8b909c6e.js"
+  }
+]);
